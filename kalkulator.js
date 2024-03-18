2079fee789e836acd24d343b7e12473da025999f
lanangwisana <lanangwisana@gmail.com>
2024-03-18 12:29:36 +0700

    v2.2 add feature perkalian

--- kalkulator.js
+++ kalkulator.js
@@ -12,6 +12,9 @@ function hitung() {
     case "-":
       hasil = parseInt(angka1) - parseInt(angka2);
       break;
+    case "*":
+      hasil = parseInt(angka1) * parseInt(angka2);
+      break;
   }
 
   document.getElementById("hasil").innerHTML = hasil;
