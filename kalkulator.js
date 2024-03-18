386ec03c283d5130d826e8490e34c73f584c6e7e
lanangwisana <lanangwisana@gmail.com>
2024-03-18 12:28:03 +0700

    v2.1 add feature pengurangan

--- kalkulator.js
+++ kalkulator.js
@@ -1,15 +1,18 @@
 function hitung() {
-    const angka1 = document.getElementById("angka1").value;
-    const angka2 = document.getElementById("angka2").value;
-    const operator = document.getElementById("operator").value;
+  const angka1 = document.getElementById("angka1").value;
+  const angka2 = document.getElementById("angka2").value;
+  const operator = document.getElementById("operator").value;
 
-    let hasil;
+  let hasil;
 
-    switch (operator) {
-        case "+":
-            hasil = parseInt(angka1) + parseInt(angka2);
-            break;
-    }
+  switch (operator) {
+    case "+":
+      hasil = parseInt(angka1) + parseInt(angka2);
+      break;
+    case "-":
+      hasil = parseInt(angka1) - parseInt(angka2);
+      break;
+  }
 
-    document.getElementById("hasil").innerHTML = hasil;
+  document.getElementById("hasil").innerHTML = hasil;
 }
