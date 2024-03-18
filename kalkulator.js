252c88895bfd2743f81aee15dd15f73d288f6f7c
lanangwisana <lanangwisana@gmail.com>
2024-03-18 12:25:53 +0700

    v2.0 add feature penambahan

---
+++ kalkulator.js
@@ -0,0 +1,15 @@
+function hitung() {
+    const angka1 = document.getElementById("angka1").value;
+    const angka2 = document.getElementById("angka2").value;
+    const operator = document.getElementById("operator").value;
+
+    let hasil;
+
+    switch (operator) {
+        case "+":
+            hasil = parseInt(angka1) + parseInt(angka2);
+            break;
+    }
+
+    document.getElementById("hasil").innerHTML = hasil;
+}
